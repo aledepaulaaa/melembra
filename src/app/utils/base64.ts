3470c8e0b53a2cd8922c2fd097e9513802b4a786
aledepaulaaa <contatoaledepaula@gmail.com>
2025-09-20 23:57:21 -0300

    fix: teste de app notificações push PWA NextJs@latest

---
+++ src/app/utils/base64.ts
@@ -0,0 +1,18 @@
+'use client'
+
+export function urlBase64ToUint8Array(base64String: string) {
+    if (typeof window === 'undefined') {
+        throw new Error('Esta função só pode ser executada no navegador');
+    }
+
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+
+    const rawData = window.atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+}
