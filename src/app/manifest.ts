c8bd70ffe8f1ced4a398955825cb1698bdd1083a
aledepaulaaa <contatoaledepaula@gmail.com>
2025-11-17 19:52:10 -0300

    fix: ajuste em manifest.ts "purpose" em 512x512

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,6 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/icon-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
+                purpose: 'maskable'
             },
         ],
     }
